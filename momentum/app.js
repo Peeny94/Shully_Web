6ad8e4f89c456149c2c0ffad8bc20d53d5691220
kyoungminLee <leegyoungmin994@gmail.com>
2024-12-02 22:31:41 +0900

    null, innerText오류가 왜 나는 지 좀 더 생각해 보자. 지금은 css를 몰라서 해결할 수 없다..ㅠ

--- momentum/app.js
+++ momentum/app.js
@@ -1,5 +1,10 @@
-const title =document.getElementById("title");
-console.dir(title);//element 요소를 좀 더 자세히 보여줌 , "" 문자 표시를 넣으면 안됨! 하나의 id값을 검색하는 것이기 때문에!!
-title.innerText ="js에 의해 HTML에 정의된 내용에 영향이 감을 알 수 있지."
-console.log(title.id);
-console.log(title.className);
+
+// <div class ="hello1" id="title">
+// <h1>중간</h1>
+// </div> 
+/*HTML상/ 하기코드로 js, null 오류가 뜸. */
+
+const title = document.querySelector(".hello1:first-child h1");
+const title2 = document.querySelector("div.hello1:first-child h1");
+title2.innerText ="CSS는 어려워";
+console.log(title2);
