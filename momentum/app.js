3f94bf92a2eef10a56e1ea8d9073c288e1716e61
kyoungminLee <leegyoungmin994@gmail.com>
2024-12-04 01:30:30 +0900

    login 기능이라기 보단 쿠키 저장 공부였다. 후에 유용하게 쓰일 듯. 이번 주말까지 웹사이트는 힘들겠지만. 노력해 보자.

--- momentum/app.js
+++ momentum/app.js
@@ -1,7 +1,9 @@
 const loginForm =document.querySelector("#login-form");
 const loginInput =document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+
+//const link = document.querySelector("a");
 
 // function onLoginBtnClick() {
 //     console.dir(loginInput);// 해당 변수에 사용되는 함수들의 목록을 볼 수 있음.
@@ -16,11 +18,33 @@ const link = document.querySelector("a");
 *}
 */
 
+//1. form이 제출되는 걸 막아줌
+//2. form className을 추가함. -> form이 실행됨 -> form이 사라짐.
+//3. h1 태그의 className을 바꿔줌
+const HIDDEN_CLASSNAME = "hidden"; //하나의 관습으로 중요하지 않은 정보지만 중복사용되기 때문에 대문자로 변수값을 지정.
+const USERNAME_KEY = "username";
+
 function onLoginSubmit(event) {
     event.preventDefault();
-    const ursename = loginInput.value;
-    loginForm.classList.add("hiddin");
+    loginForm.classList.add(HIDDEN_CLASSNAME);//className 중 .hidden 기능의 css적용 추가함.
+    const username = loginInput.value; 
+    localStorage.setItem(USERNAME_KEY, username);
+    // console.log(username);
+    //`${변수명}` : 문자열 안에 변수값을 넣을 수 있게 쌍따옴표 대신 백틱을 사용하는 게 깔끔할 수도 있다.
+    paintGreetings(username);
 }
 
+function paintGreetings(username) {
+    greeting.innerText =`Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME); //= "Hello " + username; 
+}
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-loginForm.addEventListener("submit", onLoginSubmit) 
+if(savedUsername == null){
+    //show the Form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else {
+    //show the Greeting
+    paintGreetings(savedUsername);
+}
