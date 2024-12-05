0ed1262979780d43d4fa29cb3538a49cb9d9bd33
kyoungminLee <leegyoungmin994@gmail.com>
2024-12-06 03:12:03 +0900

    vite, webpack같은 bundller 사용 또는 html상에 css 경로 입력필요.

--- subject/main.js
+++ subject/main.js
@@ -1,11 +1,10 @@
 // <⚠️ DONT DELETE THIS ⚠️>
-import './styles.css';
+//import './styles.css';
 
 const colors = ['#1abc9c', '#3498db', '#9b59b6', '#f39c12', '#e74c3c'];
 // <⚠️ /DONT DELETE THIS ⚠️>
 
 const title = document.querySelector('h2');
-title.style.color = colors[1];
 
 const superEventHandler = {
   Mouseover: function () {
@@ -21,13 +20,11 @@ const superEventHandler = {
     title.style.color = colors[1];
   },
   MouseRightclicked: function (event) {
-    event.preventDefault(); // 기본 우클릭 메뉴를 막음
     title.innerText = 'Mouse Right Clicked';
     title.style.color = colors[0];
   },
 };
 
-// 이벤트 리스너 등록
 title.addEventListener('mouseenter', superEventHandler.Mouseover);
 title.addEventListener('mouseleave', superEventHandler.Mouseout);
 window.addEventListener('resize', superEventHandler.resized);
