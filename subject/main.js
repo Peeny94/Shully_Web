cd0de2c84473832095de27f8829f4fa348b61d82
kyoungminLee <leegyoungmin994@gmail.com>
2024-12-07 04:36:05 +0900

    좀 더 효율적인 코드가 있을 것만 같다..!

--- subject/main.js
+++ subject/main.js
@@ -1,9 +1,8 @@
 //import './styles.css';// html에 직접 입력함.
 
-const hello = document.querySelector('.hello div');//=#hello, #='id'or 'class'를 의미함.
+const hello = document.querySelector('body');//=#hello, #='id'or 'class'를 의미함.
 
-const heightChange = window.innerHeight;
-const widthChange = window.innerWidth;
+let widthChange = window.innerWidth;
 /*참고
 console.log(parent.innerWidth);// 가장 가까운 프레임셋의 뷰포트의 너비를 기록합니다.
 console.log(top.innerWidth);// 가장 먼 프레임셋의 뷰포트의 너비를 기록합니다.
@@ -11,22 +10,45 @@ onresized
 onload
 
 */
-console.log(window.frames);
-console.log(window);
+// console.log(window.frames);
+// console.log(window);
+// const superHadeler = {
+//   windowResized: function (e) {
+//     //e.preventDefault();
+//     if (widthChange > 800) {
+//       e.preventDefault();
+//       hello.classList.toggle('windowLarge'); //puple
+//     } else if (500 <= widthChange && widthChange <= 800) {
+//       hello.classList.toggle('windowMiddle'); //yellow
+//     } else if (widthChange < 500) {
+//       hello.classList.toggle('windowSmall'); //red
+//     }
+//   },
+// };
+// console.dir(hello);
+// console.log(window.frames.innerHeight, window.frames.innerWidth);
+// window.addEventListener('resize', superHadeler.windowResized);
+
+
+// const hello = document.querySelector('body'); // body 요소 선택
+
+//import './styles.css';
+
+
 const superHadeler = {
-  resizedCalcul: function(e) {
-    e.preventDefault();
-    if (heightChange==window.outerHeight){
-      document.body.style.backgroundColor = 'purple';
+  windowResized: function () {
+    let widthChange = window.innerWidth;
+
+    if (widthChange > 800) {
+      hello.classList.toggle('windowLarge'); //puple
+    } else if (500 <= widthChange && widthChange <= 800) {
+      hello.classList.toggle('windowMiddle'); //yellow
+    } else if (widthChange < 500) {
+      hello.classList.toggle('windowSmall'); //red
     }
-
   },
-  windowResized: function(){
-    document.body.style.backgroundColor = 'tomato';
-    
-  } 
-}
+};
 
-console.log(window.frames.innerHeight, window.frames.innerWidth);
-window.addEventListener('load', superHadeler.resizedCalcul);
+superHadeler.windowResized();
 window.addEventListener('resize', superHadeler.windowResized);
+
