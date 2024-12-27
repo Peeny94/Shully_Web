bc30681bd1aad0ff3082e0ce3c7afc65b7a9e07d
kyoungminLee <leegyoungmin994@gmail.com>
2024-12-28 04:56:42 +0900

    타입스크립트를 이제서야 그 존재감을 인식헀다. 타입 검증은 앞으로 자바로 대체하는 걸로.

--- shully_web/src/components/timeline.js
+++ shully_web/src/components/timeline.js
@@ -1,19 +1,20 @@
 import { useEffect, useState } from "react";
 import { db } from "../firebase";
-
+import PropTypes from "prop-types";
 import { collection, getDocs, orderBy, query } from "firebase/firestore";
 import { Wrapper } from "./auth-Components";
-
-export interface IShully {
-    photo: String;
-    shully: String;
-    userid: String;
-    username: String;
-    createdAt: Number;
-}
+//.tsx 구조에선 typescript 를 써서 하기 코드만으로 타입 검증이 된다고 한다. 난 그냥. js도 잘 못하는데, ts를 쓰기보단 자바에 집중하는 게 좋다. 하기에 props. 으로 해서 프로퍼티 자식요소를 검증하는 방식을 쓴다.
+// export interface IShully {
+//     photo: String;
+//     shully: String;
+//     userid: String;
+//     username: String;
+//     createdAt: Number;
+// }
 
 export default function Timeline() {
-    const [shullys, setShully] = useState<IShully>([]); 
+    //  const [tweets, setTweet] = useState<ITweet[]>([]); <- 기존 코드.
+    const [shullys, setShully] = useState([]); // 해당 부분에서 타입 정의를 하지 않기로 해..
     const fetchShullys = async() => {
         const shullysQuery = query(
             collection(db, "shullys"),
@@ -25,7 +26,22 @@ export default function Timeline() {
     useEffect(()=> {
         fetchShullys();
     },[]);
+// 타입 검증은 코드 안정성 및 에러 확인에 중요.
+    Timeline.propTypes = {
+        shullys: PropTypes.arrayOf(
+            // 배열의 구조를 잡아준다. 안정성 확보에 좋다.
+          PropTypes.shape({
+            photo: PropTypes.string,
+            shully: PropTypes.string.isRequired,
+            userid: PropTypes.string.isRequired,
+            username: PropTypes.string.isRequired,
+            createdAt: PropTypes.number.isRequired,
+          })
+        ),
+      };
+
     return <Wrapper>
         {JSON.stringify(shullys)}
     </Wrapper>;
 }
+
